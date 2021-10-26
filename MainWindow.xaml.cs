9cadb8597520cc60f12af419435fdf88e7a756ab
tszymanski98 <tomasz.szymanski.tsz@gmail.com>
2021-10-26 20:51:31 +0200

    Dodawanie-Ściąganie z pliku - do przerobienia na CSV

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace WpfLab3
 {
@@ -20,9 +22,11 @@ namespace WpfLab3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Article> ity = new List<Article>();
         public MainWindow()
         {
             InitializeComponent();
+            datagrid1.ItemsSource = ity;
             ///           Testowe artykuły:
             ///           datagrid1.Items.Add(new Article("Cola", 1, 4));
             ///           datagrid1.Items.Add(new Article("Fanta", 2, 90));
@@ -32,26 +36,54 @@ namespace WpfLab3
         {
             public string NameDat { get; set; }
 
-            public int IDDat { get; set; }
+            public string IDDat { get; set; }
 
             public int CountDat { get; set; }
 
+            /*
             public Article(string Name, int ID, int Count)
             {
                 NameDat = Name;
                 IDDat = ID;
                 CountDat = Count;
-
             }
+            */
+        }
+
+        public void DodArt(string artykul, string ilosc)
+        {
+            int index = ity.Count + 1;
+            ity.Add(new Article()
+            {NameDat = artykul,IDDat = index.ToString(),CountDat = int.Parse(ilosc)});
+            datagrid1.Items.Refresh();
         }
 
         private void zapis_but(object sender, RoutedEventArgs e)
         {
+            string saveText = "";
+            foreach(Article art in ity)
+            {
+                saveText += $"{art.NameDat},{art.IDDat},{art.CountDat}\n";
+            }
 
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog()==true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, saveText);
+            }
         }
         private void odczyt_but(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog()==true)
+            {
+                ity.Clear();
+                foreach(string line in File.ReadLines(openFileDialog.FileName))
+                {
+                    string[] args = line.Split(',');
+                    DodArt(args[0], args[2]);
+                }
+            }
         }
 
         private void dod_but(object sender, RoutedEventArgs e)
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -24,9 +24,23 @@ namespace WpfLab3
             InitializeComponent();
         }
 
+        public string Art = "";
+        public string Ilo = "";
+
         private void dodaj_but(object sender, RoutedEventArgs e)
         {
+            ((MainWindow)Application.Current.MainWindow).DodArt(Art, Ilo);
+        }
 
+        private void Art_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            Art = tb.Text;
+        }
+        private void Ilo_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            Ilo = tb.Text;
         }
     }
 }
